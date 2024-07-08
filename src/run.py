63eb26d3d1729b47aa50a0162b584d0f2858d8ee
Aryo Gema <aryopg@gmail.com>
2024-07-08 17:20:22 +0100

    cast int64 to int

--- src/run.py
+++ src/run.py
@@ -125,7 +125,7 @@ class Run:
 
             predictions.append(batch)
 
-            batch["idx"] = batch["idx"].cpu().numpy()[0]
+            batch["idx"] = int(batch["idx"].cpu().numpy()[0])
             print(batch)
             for key, value in batch.items():
                 print(key, ":", value, type(value))
