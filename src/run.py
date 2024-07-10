756011d7944ca7e3200f2539cbb9957e32032f99
Aryo Gema <aryopg@gmail.com>
2024-07-10 14:43:46 +0100

    ready for full memo trap run

--- src/run.py
+++ src/run.py
@@ -127,13 +127,9 @@ class Run:
             except:
                 batch["idx"] = str(batch["idx"][0])
 
-            print(batch)
-
             # Save the predictions to a JSONL file after each batch
             with open(prediction_filepath, "a") as f:
                 f.write(json.dumps(batch) + "\n")
-
-            break
 
         # Evaluate
         metrics = self.metrics(predictions)
