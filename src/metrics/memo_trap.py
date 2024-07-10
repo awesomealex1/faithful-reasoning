24d314c1d8c6b30bf5119ca33d9524779be15a74
Aryo Gema <aryopg@gmail.com>
2024-07-10 14:23:08 +0100

    debug memotrap

--- src/metrics/memo_trap.py
+++ src/metrics/memo_trap.py
@@ -12,9 +12,11 @@ class MemoTrap:
         for sample in predictions:
             scores_true = sample["scores_true"][0]
             scores_false = sample["scores_false"][0]
+            split = sample["split"][0]
             print(scores_true)
             print(scores_false)
-            scores[sample["split"]] += [1.0 if scores_true > scores_false else 0.0]
+            print(sample["split"])
+            scores[split] += [1.0 if scores_true > scores_false else 0.0]
 
         metrics = {f"{split}_acc": np.mean(scores[split]) for split in scores.keys()}
         metrics["macro_avg_acc"] = np.mean(list(metrics.values()))
