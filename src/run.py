e3cc9ae2ddf75e9b43220a4c1b1e55c4ed2ea9a7
Aryo Gema <aryopg@gmail.com>
2024-07-11 20:40:39 +0100

    ready for full run

--- src/run.py
+++ src/run.py
@@ -106,8 +106,6 @@ class Run:
             with open(prediction_filepath, "a") as f:
                 f.write(json.dumps(batch) + "\n")
 
-            break
-
         # Evaluate
         metrics = self.metrics(predictions)
 
