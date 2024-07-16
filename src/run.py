0cba4561194edbb85927c68204774091d64f5ce5
Aryo Gema <aryopg@gmail.com>
2024-07-16 10:21:17 +0100

    debugging

--- src/run.py
+++ src/run.py
@@ -107,6 +107,9 @@ class Run:
             with open(prediction_filepath, "a") as f:
                 f.write(json.dumps(batch) + "\n")
 
+            if step >= 10:
+                break
+
         # Evaluate
         metrics = self.metrics(predictions)
 
