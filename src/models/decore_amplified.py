b51f570c67a275bff8f580609cf523c3904c4fdf
Aryo Gema <aryopg@gmail.com>
2024-07-18 10:33:52 +0100

    decore amplified

--- src/models/decore_amplified.py
+++ src/models/decore_amplified.py
@@ -1,6 +1,7 @@
 from typing import List, Optional, Tuple
 
 import copy
+import random
 import os
 import json
 import torch
@@ -12,7 +13,7 @@ from src.configs import DecoderConfigs, ModelConfigs
 from src.models.base_model import BaseModel
 
 
-class DeCoReVanilla(BaseModel):
+class DeCoReAmplified(BaseModel):
     def __init__(
         self,
         model_configs: ModelConfigs,
@@ -20,12 +21,13 @@ class DeCoReVanilla(BaseModel):
     ):
         super().__init__(model_configs, decoder_configs)
 
-        self._load_retrieval_heads()
+        self.num_retrieval_heads = self.decoder_configs.configs.num_retrieval_heads
+        self.retrieval_heads = self._load_retrieval_heads()
+        self.random_heads = self._construct_random_head()
         print("Retrieval heads: ", self.retrieval_heads)
+        print("Random heads: ", self.random_heads)
 
     def _load_retrieval_heads(self):
-        self.num_retrieval_heads = self.decoder_configs.configs.num_retrieval_heads
-
         model_base_name = self.model_configs.configs.model_name_or_path.split("/")[1]
 
         with open(
@@ -38,9 +40,23 @@ class DeCoReVanilla(BaseModel):
 
         stable_block_list = [(l[0], np.mean(l[1])) for l in head_list.items()]
         stable_block_list = sorted(stable_block_list, key=lambda x: x[1], reverse=True)
-        self.retrieval_heads = [
-            [int(ll) for ll in l[0].split("-")] for l in stable_block_list
-        ][: self.num_retrieval_heads]
+        return [[int(ll) for ll in l[0].split("-")] for l in stable_block_list][
+            : self.num_retrieval_heads
+        ]
+
+    def _construct_random_head(self):
+        results = []
+        seed_list = [
+            i for i in range(32)
+        ]  # FIXME: 32 is hardcoded, copied from Retrieval_Head repo
+        random.shuffle(seed_list)
+        while len(results) < self.num_retrieval_heads:
+            l, h = random.choices(seed_list, k=2)
+            if (l, h) in results or (l, h) in self.retrieval_heads:
+                continue
+            else:
+                results.append((l, h))
+        return results
 
     def generate(
         self,
@@ -58,7 +74,8 @@ class DeCoReVanilla(BaseModel):
             generated_ids = []
             last_input_token = inputs[:, -1]
             base_past_kv = copy.deepcopy(input_logits.past_key_values)
-            hallucinated_past_kv = copy.deepcopy(input_logits.past_key_values)
+            random_mask_past_kv = copy.deepcopy(input_logits.past_key_values)
+            retrieval_mask_past_kv = copy.deepcopy(input_logits.past_key_values)
             for _ in range(self.max_new_tokens):
                 last_input_token = last_input_token.view(1, 1)
 
@@ -68,24 +85,33 @@ class DeCoReVanilla(BaseModel):
                     use_cache=True,
                     attn_mode="torch",
                 )
-                hallucinated_outputs = self.model(
+                random_mask_outputs = self.model(
                     input_ids=last_input_token,
-                    past_key_values=hallucinated_past_kv,
+                    past_key_values=random_mask_past_kv,
+                    use_cache=True,
+                    attn_mode="torch",
+                    block_list=self.random_heads,
+                )
+                retrieval_mask_outputs = self.model(
+                    input_ids=last_input_token,
+                    past_key_values=retrieval_mask_past_kv,
                     use_cache=True,
                     attn_mode="torch",
                     block_list=self.retrieval_heads,
                 )
 
                 base_past_kv = base_outputs.past_key_values
-                hallucinated_past_kv = hallucinated_outputs.past_key_values
+                random_mask_past_kv = random_mask_outputs.past_key_values
+                retrieval_mask_past_kv = retrieval_mask_outputs.past_key_values
 
-                next_token_logits = (
-                    1 + self.decoder_configs.configs.alpha
-                ) * base_outputs.logits[
+                # Random masked output is supposed to be more faithful than retrieval masked output
+
+                next_token_logits = base_outputs.logits[
                     0, -1
-                ] - self.decoder_configs.configs.alpha * hallucinated_outputs.logits[
-                    0, -1
-                ]
+                ] + self.decoder_configs.configs.alpha * (
+                    random_mask_outputs.logits[0, -1]
+                    - retrieval_mask_outputs.logits[0, -1]
+                )
 
                 last_input_token = next_token_logits.argmax()
                 generated_ids.append(last_input_token.item())
