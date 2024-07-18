585c005c93dfe15cbed4cee9acd94082c23cef1b
Aryo Gema <aryopg@gmail.com>
2024-07-18 11:06:57 +0100

    import to root

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,5 +3,7 @@ from .baseline_masked_retrieval_head import BaselineMaskedRetrievalHead
 from .baseline_masked_non_retrieval_head import BaselineMaskedNonRetrievalHead
 from .contrastive_decoding import ContrastiveDecoding
 from .decore_vanilla import DeCoReVanilla
+from .decore_amplified import DeCoReAmplified
+from .decore_plain import DeCoRePlain
 from .dola import DoLa
 from .icd import ICD
