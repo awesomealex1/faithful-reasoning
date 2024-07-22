f595edfaa11aae5fbb530c4094a45bcfd794202c
Aryo Gema <aryopg@gmail.com>
2024-07-22 15:24:38 +0100

    debug

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -28,11 +28,15 @@ class Baseline(BaseModel):
         print(inputs)
         if self.model_configs.model_type == "instruct":
             bos_length = 1
-            question_length = self._verbalise_input(inputs["question"]).shape[-1]
+            question_length = self._verbalise_input(
+                inputs["verbalised_question"]
+            ).shape[-1]
             context_length = tokenised_inputs.size(1) - question_length - bos_length
         else:
             bos_length = 1
-            question_length = self._verbalise_input(inputs["question"]).shape[-1]
+            question_length = self._verbalise_input(
+                inputs["verbalised_question"]
+            ).shape[-1]
             context_length = tokenised_inputs.size(1) - question_length - bos_length
         print("bos_length: ", bos_length)
         print("question_length: ", question_length)
