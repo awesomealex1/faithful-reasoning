0e04fea83540ba6ebfce84b3614932efe3a533f0
Aryo Gema <aryopg@gmail.com>
2024-07-31 15:44:02 +0100

    contrastive guided decoding

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,7 @@ from .baseline_masked_honest_head import BaselineMaskedHonestHead
 from .baseline_masked_retrieval_head import BaselineMaskedRetrievalHead
 from .baseline_masked_non_retrieval_head import BaselineMaskedNonRetrievalHead
 from .contrastive_decoding import ContrastiveDecoding
+from .contrastive_guided import ContrastiveGuided
 from .decore_vanilla import DeCoReVanilla
 from .decore_amplified import DeCoReAmplified
 from .decore_plain import DeCoRePlain
