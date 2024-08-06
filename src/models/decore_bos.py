65795e0b57a1197bfa7ec396e9c23f3028fa68f4
Aryo Gema <aryopg@gmail.com>
2024-08-06 14:36:03 +0100

    debug

--- src/models/decore_bos.py
+++ src/models/decore_bos.py
@@ -54,15 +54,15 @@ class DeCoReBOS(BaseModel):
             # Calculate attention for bos and non bos
             print("attentions[l].shape: ", attentions[l].shape)
             bos_attn = attentions[l][0, :, -1, 0]
-            non_bos_context_attn = attentions[l][0, :, -1, 1:].mean(-1)
+            # non_bos_context_attn = attentions[l][0, :, -1, 1:].mean(-1)
 
             print("bos_attn.shape: ", bos_attn.shape)
-            print("non_bos_context_attn.shape: ", non_bos_context_attn.shape)
+            # print("non_bos_context_attn.shape: ", non_bos_context_attn.shape)
 
             print("bos_attn: ", bos_attn)
-            print("non_bos_context_attn: ", non_bos_context_attn)
+            # print("non_bos_context_attn: ", non_bos_context_attn)
 
-            bos_lookback_ratio[l, :] = bos_attn / (bos_attn + non_bos_context_attn)
+            bos_lookback_ratio[l, :] = bos_attn  # / (bos_attn + non_bos_context_attn)
             print("bos_lookback_ratio[l, :]: ", bos_lookback_ratio[l, :])
 
         mean_bos_lookback_ratio = torch.mean(bos_lookback_ratio)
