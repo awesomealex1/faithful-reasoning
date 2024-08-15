a1c76f78fdb9923492cff893074efaddfed9fd67
Aryo Gema <aryopg@gmail.com>
2024-08-15 18:01:35 +0100

    debug nq swap (just cautionary)

--- src/models/decore_entropy.py
+++ src/models/decore_entropy.py
@@ -65,7 +65,9 @@ class DeCoReEntropy(BaseModel):
         ), "Return attentions not supported for DeCoReVanilla"
         self.model.eval()
 
+        print(inputs)
         prompt = inputs["prompted_question"][0]
+        print(prompt)
         inputs = self._verbalise_input(prompt).to(self.model.device)
 
         # Predict
