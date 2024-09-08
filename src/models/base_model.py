df1b60ddbac8ba387a2401c6456cb2c76dedfc32
Aryo Gema <aryopg@gmail.com>
2024-09-08 22:23:32 +0100

    debug mistral low performance on popqa

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -130,6 +130,7 @@ class BaseModel(ABC):
                     if type(inputs) in [tuple, list]:
                         inputs = inputs[0]
                     chat_inputs += [{"role": "user", "content": inputs}]
+                print(chat_inputs)
                 inputs = tokenizer.apply_chat_template(
                     chat_inputs,
                     add_generation_prompt=add_generation_prompt,
