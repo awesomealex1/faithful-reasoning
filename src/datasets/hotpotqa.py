5f8d76452730aff43743e6438ad4e079da914e85
Alexander Murphy <lordpizza11@gmail.com>
2025-02-03 22:05:20 +0000

    Add 2Wiki and Hotpot datasets

---
+++ src/datasets/hotpotqa.py
@@ -0,0 +1,29 @@
+from base_dataset import BaseDataset
+from configs import DataConfigs
+from typing import List
+import os
+
+class HotpotQA(BaseDataset):
+
+    def __init__(
+        self,
+        data_configs: DataConfigs,
+        **kwargs,
+    ):
+        super().__init__(data_configs, **kwargs)
+        self.variation = data_configs.variation
+
+        self.data_filename = os.path.join(self.data_dir, "dev_subsampled.jsonl")
+
+        # Prepare data
+        self.data = self.parse_data()
+
+    def parse_data(self) -> List[dict]:
+        pass
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        return sample
+
+    def __len__(self):
+        return len(self.data)
