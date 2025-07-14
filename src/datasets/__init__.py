2290383dc13a79c6a9012e425073bc2d004faef3
Alexander Murphy <lordpizza11@gmail.com>
2025-07-14 18:02:22 +0200

    ALFWorld commit

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .musique import MuSiQue
 from .hotpotqa import HotpotQA
 from .wiki import WikiMultihopQA
+from .alfworld import ALFWorldDataset
---
+++ src/datasets/alfworld.py
@@ -0,0 +1,46 @@
+import torch
+from torch.utils.data import Dataset
+from src.configs import DataConfigs
+
+import alfworld.agents.environment as alf_env
+import alfworld.agents.modules.generic as generic
+import numpy as np
+
+class ALFWorldDataset(Dataset):
+    def __init__(self, data_configs: DataConfigs, **kwargs):
+        self.data_configs = data_configs
+        self.num_episodes = data_configs.num_samples if data_configs.num_samples > 0 else 100  # default
+        self.batch_size = kwargs.get('batch_size', 1)
+        self.config = generic.load_config()
+        self.env_type = 'AlfredTWEnv'  # text-based
+        self.env = alf_env.get_environment(self.env_type)(self.config, train_eval='train')
+        self.env = self.env.init_env(batch_size=self.batch_size)
+        self.episodes = []
+        self._generate_episodes()
+
+    def _generate_episodes(self):
+        for _ in range(self.num_episodes):
+            obs, info = self.env.reset()
+            done = [False] * self.batch_size
+            episode = []
+            while not all(done):
+                admissible_commands = list(info['admissible_commands'])
+                # For now, take random actions from admissible commands
+                actions = [np.random.choice(cmds) if len(cmds) > 0 else 'look' for cmds in admissible_commands]
+                next_obs, scores, done, info = self.env.step(actions)
+                episode.append({
+                    'observation': obs,
+                    'action': actions,
+                    'reward': scores,
+                    'done': done,
+                    'info': info
+                })
+                obs = next_obs
+            self.episodes.append(episode)
+
+    def __getitem__(self, idx):
+        # Return the full episode for now
+        return self.episodes[idx]
+
+    def __len__(self):
+        return len(self.episodes) 
